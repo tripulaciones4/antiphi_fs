059da87d0a9fcbaa074f07f4b4c40167d8588663
Sara <lopezlovonsara@gmail.com>
2022-01-15 19:27:00 +0100

    add first instance of JWT

--- server/index.js
+++ server/index.js
@@ -1,18 +1,85 @@
 const express = require('express')
 require('dotenv').config() // Carga fichero variables de entorno
 const sequelize= require("./utils/db.js");
-const companiesRouter = require('./routes/companies')
-const queriesRouter = require('./routes/queries')
-const usersRouter = require('./routes/users')
+const companiesRouter = require('./routes/companies');
+const queriesRouter = require('./routes/queries');
+const usersRouter = require('./routes/users');
 
-const app = express()
+const app = express();
+const jwt = require('jsonwebtoken');
 const port = 4000 //3000 para el front y 4000 para el back
 
+app.use(express.urlencoded({extended:false}))
 app.use(express.json()) // Para habilitar envio de JSON al servidor
 
 app.use("/api/companies/", companiesRouter)
 app.use("/api/queries/", queriesRouter) 
-app.use("/api/users/", usersRouter)  
+app.use("/api/users/", usersRouter)
+
+//Prueba JWT
+app.get("/", (req,res)=>{
+  res.send('Hola mundo')
+})
+
+app.get("/login", (req,res)=>{
+  res.send(`<html>
+              <head>
+                <title>Login</title>
+              </head>
+              <body>
+                <form method="POST" action="/auth">
+                  Nombre: <input type="text" name="username"><br>
+                  Contraseña: <input type="password" name="password"><br>
+                  <input type="submit" value="Iniciar sesión"/>
+                </form>
+              </body>
+            </html>`)
+})
+
+app.get("/auth", (req,res)=>{  
+  const {username, password} = req.body;
+  //Ahora tendriamos que consultar a la BBDD y ver que existen
+  const user = {username: username}; //Aqui podriamos poner cualquier info
+
+  const accessToken = generateAccessToken(user);
+  
+  res.header('authorization', accessToken).json({
+    message:"Usuario autenticado",
+    token: accessToken
+  })
+
+});
+ 
+const generateAccessToken = (user)=>{
+  return jwt.sign(user, process.env.SECRET, {expiresIn: '5m'}) 
+}
+
+const validateToken = (req, res, next) => {
+
+  const accessToken = req.headers['authorization'] || req.query.accesstoken;
+  if(!accessToken) res.send('Access denied');
+  
+  jwt.verify(accessToken,process.env.SECRET,(err,user)=>{
+      if(err){
+        res.send('Access denied, token expired or incorrect')
+      }else{
+        req.user = user;
+        next();
+      }
+    })
+}
+
+app.get("/api", validateToken, (req,res) =>{
+  res.json({
+    username: req.user,
+    tuits:[
+      {id:0,
+     text:'Primer ejemplo',
+     username:'Pipi'},
+     {id:1,
+       text:'Segundo ejemplo',
+       username:'Pipi'}]})
+ })
 
 
 //Listen
