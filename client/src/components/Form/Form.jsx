bf07fcb7da7674989bf8a396171ce1de2a776aa3
Sara <lopezlovonsara@gmail.com>
2022-01-17 13:43:34 +0100

    Add changes

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import "./Form.css";
 import Header from "../Header/Header";
 import Searches from "../Searches/Searches";
+import fondo  from '../../assets/fondo.jpeg';
 
 
 const Form = () => {
     return (
         <div>
             <div><Header /></div>
-            <form action="" method="POST">
-                <label htmlFor="search">Don't take the bait!</label>                    
-                <input type="text" name="search" placeholder="Buscar URL" />                    
-                <button type="submit" value="Submit">Buscar</button>
+
+            <form className="form-search" action="" method="POST" style={{ backgroundImage: `url(${fondo})` }}>
+                <label className="label-search" htmlFor="search">Don't take the bait!</label>                    
+                <div className="search-container">
+                    <input className="input-search" type="text" name="search" placeholder="Buscar URL" />                    
+                    <button className="btn-search" type="submit" value="Submit">Buscar</button>
+                </div>
             </form>
+
             <div><Searches /></div>
         </div>      
     );
