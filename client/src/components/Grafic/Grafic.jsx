e201a83d35090ce0868ae00fc19ef313a0d2bb5a
Sara <lopezlovonsara@gmail.com>
2022-01-19 02:00:07 +0100

    Add style page reporting

---
+++ client/src/components/Grafic/Grafic.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Grafic.css";
+
+const Grafic = () => {
+  return <div className="grafic-container">
+    
+    <div className="title-grafic">
+      <h2>Búsquedas</h2>
+      <p>350</p>
+    </div>
+
+    <div className="elem-grafic">Añadir el gráfico</div>
+
+    </div>;
+};
+
+export default Grafic;
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../Login/Login";
 import Form from "../Form/Form";
+import Reporting from "../Reporting/Reporting";
 import "./Main.css"
 import {Route, Routes} from 'react-router-dom'; //Para las rutas
 
@@ -10,7 +11,8 @@ const Main = () => {
   return (<main>  
   <Routes>    
     <Route path="/" element={<Login />} />    
-    <Route path="/home" element={<Form />} />    
+    <Route path="/home" element={<Form />} />
+    <Route path="/reporting" element={<Reporting />} />    
   </Routes>
 
   </main>);
